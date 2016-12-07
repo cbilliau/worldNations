1332599b7b6b81a67234e7f2cf2b1eda9fc95dea
cbilliau <christopher@billiau.com>
2016-12-07 15:27:30 -0500

    Refactored CountryRow.js return to iterate over var holding key names to display data. Need to put in an if statement for calling 'numberfy' function module

--- src/components/CountryRow.js
+++ src/components/CountryRow.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { numberfy } from '../utils';
+import React, {Component} from 'react';
+import {numberfy} from '../utils';
 
 import './CountryRow.css';
 
@@ -20,20 +20,40 @@ class CountryRow extends Component {
     }
 
     render() {
+        const countryProps = [
+            'alpha2Code',
+            'capital',
+            'population',
+            'area',
+            'timezones',
+            'languages'
+        ];
+
         let country = this.props.country;
+
         return (
-            <tr onClick={ () => this.handleClick() } className='CountryRow'>
-              <td>{country.region}</td>
-              <td>{country.name}</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={country.alpha2Code}/>: null}</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={country.capital}/>: null}</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={country.population}/>: null}</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={country.area}/> : null}</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={ numberfy(country.timezones) }/> : null }</td>
-              <td>{this.state.renderDetails ? <CountryDetail country={country.languages}/>: null}</td>
+            <tr onClick={() => this.handleClick()} className='CountryRow'>
+                <td>{country.region}</td>
+                <td>{country.name}</td>
+                {countryProps.map(countryProp => <td>{this.state.renderDetails
+                        ? <CountryDetail country={country[countryProp]}/>
+                        : null}</td>)}
             </tr>
         );
     }
+    // let country = this.props.country;
+    // return (
+    //     <tr onClick={ () => this.handleClick() } className='CountryRow'>
+    //       <td>{country.region}</td>
+    //       <td>{country.name}</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={country.alpha2Code}/>: null}</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={country.capital}/>: null}</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={country.population}/>: null}</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={country.area}/> : null}</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={ numberfy(country.timezones) }/> : null }</td>
+    //       <td>{this.state.renderDetails ? <CountryDetail country={country.languages}/>: null}</td>
+    //     </tr>
+    // );
 };
 
 export default CountryRow;
