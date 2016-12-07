7f6088f6b2b139857067b5d0a731965b62f6e5e7
cbilliau <christopher@billiau.com>
2016-12-07 15:40:54 -0500

    deleted old code

--- src/components/CountryRow.js
+++ src/components/CountryRow.js
@@ -41,19 +41,6 @@ class CountryRow extends Component {
             </tr>
         );
     }
-    // let country = this.props.country;
-    // return (
-    //     <tr onClick={ () => this.handleClick() } className='CountryRow'>
-    //       <td>{country.region}</td>
-    //       <td>{country.name}</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={country.alpha2Code}/>: null}</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={country.capital}/>: null}</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={country.population}/>: null}</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={country.area}/> : null}</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={ numberfy(country.timezones) }/> : null }</td>
-    //       <td>{this.state.renderDetails ? <CountryDetail country={country.languages}/>: null}</td>
-    //     </tr>
-    // );
 };
 
 export default CountryRow;
